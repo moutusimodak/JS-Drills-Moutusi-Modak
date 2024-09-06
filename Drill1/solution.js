76c6916a6af203bbbc82736d8f789c24431e0a4f
moutusi <moutusi.slg01@gmail.com>
2024-09-06 11:13:20 +0530

    question 1 done

---
+++ Drill1/solution.js
@@ -0,0 +1,13 @@
+const arrayOfObjects = require('./persons.js');
+
+//  Question -1
+function getEmail(){
+
+    const emails =[]
+        for (let i = 0; i < arrayOfObjects.length; i++) {   
+            emails.push(arrayOfObjects[i].email);
+        }    
+       return emails;
+    }
+
+module.exports = { getEmail};
