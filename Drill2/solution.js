3560e2f4db52362cad12333ac52a835d1af86a49
moutusi <moutusi.slg01@gmail.com>
2024-09-06 11:29:31 +0530

    problem 1 done

---
+++ Drill2/solution.js
@@ -0,0 +1,31 @@
+const users = require('./userdata');
+//Question 1
+function isGame() {
+
+    const regex= /\bVideo\s*Games\b/i;
+    const userList =[]
+
+    for(const key in users){
+        console.log(`user is coming: ${key}`);
+       let interestlist = users[key].interests 
+       console.log(interestlist);
+
+      for (let i = 0; i < interestlist.length; i++) {
+        
+        if (regex.test(interestlist[i])) {
+
+            userList.push(key);
+            break;
+         } 
+        
+      }
+            
+    }
+    return userList;
+}
+
+
+module.exports = {
+   
+    isGame 
+};
---
+++ Drill2/test.js
@@ -0,0 +1,6 @@
+const {
+    isGame
+   
+} = require('./solution');
+
+console.log("Users interested in playing video games:", isGame());
