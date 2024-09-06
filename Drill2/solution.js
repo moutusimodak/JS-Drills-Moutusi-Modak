e438e996905a23b7c6a282d83894337d41fdac8c
moutusi <moutusi.slg01@gmail.com>
2024-09-06 11:45:22 +0530

    did few changes in solution file

--- Drill2/solution.js
+++ Drill2/solution.js
@@ -1,11 +1,14 @@
 const users = require('./userdata');
+
 //Question 1
+
 function isGame() {
 
     const regex= /\bVideo\s*Games\b/i;
     const userList =[]
 
     for(const key in users){
+
         console.log(`user is coming: ${key}`);
        let interestlist = users[key].interests 
        console.log(interestlist);
@@ -21,35 +24,45 @@ function isGame() {
       }
             
     }
+
     return userList;
 }
 
 // Question 2
 function isInGermany(place) {
+
     const usersInGermany = [];
+
     for (const key in users) {
+
         if (users[key].nationality === place) {
             usersInGermany.push(key);
         }
     }
+
     return usersInGermany;
 }
 
 // Question 3
 
 function isMasterDegree(degree) {
+
     const userlist = [];
+
     for (const key in users) {
+
         if (users[key].qualification === degree) {
             userlist.push(key);
         }
     }
+
     return userlist;
 }
 
 // Question 4
 
 function programmingLanguageGroup() {
+
     let languageGroups = {
         'golang': [],
         'javascriptDeveloper': [],
@@ -61,6 +74,7 @@ function programmingLanguageGroup() {
     const pythonRegex = /Python\s*Developer/i;
 
     for (const key in users) {
+        
         let designation = users[key].desgination;
 
         if (golangRegex.test(designation)) {
--- Drill2/test.js
+++ Drill2/test.js
@@ -4,6 +4,9 @@ const {
 } = require('./solution');
 
 console.log("Users interested in playing video games:", isGame());
+
 console.log(`all users staying in  Germany`, isInGermany('Germany'));
+
 console.log("Users with Master's Degree:", isMasterDegree("Masters"));
+
 console.log("Users grouped by programming language:", programmingLanguageGroup());
