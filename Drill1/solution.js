7fa5077e48eb42a7d5a75aa22ab9d95b2f83113c
moutusi <moutusi.slg01@gmail.com>
2024-09-06 11:15:36 +0530

    question 2 done

--- Drill1/solution.js
+++ Drill1/solution.js
@@ -10,4 +10,28 @@ function getEmail(){
        return emails;
     }
 
-module.exports = { getEmail};
+// Question -2
+function getHobby( age){
+    if (age === undefined || age === null) {
+        return "Please enter an age.";
+    }
+   
+    let found = false
+    const hobby =[]
+        for (let i = 0; i < arrayOfObjects.length; i++) {
+            
+            if(arrayOfObjects[i].age === age){
+                hobby.push(arrayOfObjects[i].hobbies);
+                found = true;  
+            }     
+        }
+       
+        if (!found) {
+            return "No age found";
+        }
+       
+       return hobby;
+    }
+
+
+module.exports = { getEmail, getHobby};
--- Drill1/test.js
+++ Drill1/test.js
@@ -1,7 +1,12 @@
 const arrayOfObjects = require('./persons');
-const {getEmail} = require("./solution")
+const {getEmail, getHobby} = require("./solution")
 
-
+//q1
 const emails = getEmail(arrayOfObjects);
  const emailList = emails.join('\n');
 console.log(`The email address of all individuals are - ${emailList}`);
+
+//q2
+
+const hobby = getHobby(30);
+console.log(`${hobby}`);
